3a363c3597dd8ee094c9f4ba70851991a96cb1c1
unknown <angel.pacsi@ucsp.edu.pe>
2023-11-27 15:53:13 -0500

    PELOTA VELOCIDAD PROGRESIVA, PERO MEDIO RARITA

--- PONG++/PONG++/Pelota.h
+++ PONG++/PONG++/Pelota.h
@@ -7,9 +7,11 @@ public:
     void updatePosition();
     void reverseX();
     void reverseY();
-    void setPosition(float x, float y);  // Nuevo método para establecer la posición
+    void setPosition(float x, float y);
     sf::Sprite getSprite();
     float getRadio();
+    void incrementarVelocidad();  // Nuevo método para incrementar la velocidad
+    void resetearVelocidad();     // Nuevo método para resetear la velocidad
 
 private:
     float x, y;
@@ -17,4 +19,5 @@ private:
     sf::Sprite pelota;
     float radio;
     sf::Texture textura;
+    float factorIncrementoVelocidad;  // Nuevo miembro para controlar el incremento de velocidad
 };
